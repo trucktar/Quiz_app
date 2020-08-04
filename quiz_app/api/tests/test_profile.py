ef3314af2dace43e08c34ce634ddb072628380af
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-08-04 18:50:00 +0300

    create user profile test

--- quiz_app/api/tests/test_profile.py
+++ quiz_app/api/tests/test_profile.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from quiz_app.api.models import User
+from quiz_app.api.models import User, Profile
 # Create your tests here.
 
 class TestUser(TestCase):
@@ -7,23 +7,8 @@ class TestUser(TestCase):
         self.user1 = User.objects.create(id=1, username='jorim', email='jorim.webdev@gmail.com', first_name='Jorim', last_name='Midumbi', password='moringa81')
         self.user2 = User.objects.create(id=2, username='doctar', email='doctar@gmail.com', first_name='Doc', last_name='Tar', password='doc420')
         self.user3 = User.objects.create(id=3, username='joekadenge', email='joekadenge@gmail.com', first_name='Joe', last_name='Kadenge', password='jdeng32')
-
-    def tearDown(self):
-        self.user1.delete()
-        self.user2.delete()
-        self.user3.delete()
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.user1, User))
-        self.assertTrue(isinstance(self.user2, User))
-        self.assertTrue(isinstance(self.user3, User))
-
-    def test_save_user(self):
-        self.assertTrue(self.user1.username == 'jorim')
-        self.assertTrue(self.user2.username == 'doctar')
-        self.assertTrue(self.user3.username == 'joekadenge')
-
-    # def test_delete_user(self):
-    #     self.user1.delete()
-    #     self.user2.delete()
-    #     self.user3.delete()
+    
+    def test_save_profile(self):
+        self.assertIsInstance(self.user1.profile, Profile)
+        self.assertIsInstance(self.user2.profile, Profile)
+        self.assertIsInstance(self.user3.profile, Profile)
