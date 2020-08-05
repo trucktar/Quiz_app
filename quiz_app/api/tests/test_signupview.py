2912adf7cc273504b4b87b1de1bc117ead88ee42
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-08-05 21:15:48 +0300

    add run signupApiView test ok

--- quiz_app/api/tests/test_signupview.py
+++ quiz_app/api/tests/test_signupview.py
@@ -1,64 +1,17 @@
 import json
+
 from django.contrib.auth.models import User
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
-from rest_framework import status
-from api.models import User
-from api.serializers import SignupSerializer
+
+from quiz_app.api.models import User
+from quiz_app.api.serializers import SignupSerializer
 
 
-class BaseTest(TestCase):
-    def setUp(self):
-        self.signup_url=reverse('SignupAPIView')
-        self.login_url=reverse('LoginAPIView')
-        self.user={
-            'email':'testemail@gmail.com',
-            'username':'username',
-            'first_name':'first',
-            'last_name':'last',
-            'password':'password'
-        }
-       
-        self.user_unmatching_password={
-
-            'email':'testemail@gmail.com',
-            'username':'username',
-            'first_name':'first',
-            'last_name':'last',
-            'password':'pssword'
-        }
-
-        self.user_invalid_email={
-            
-            'email':'test.com',
-            'username':'username',
-            'first_name':'first',
-            'last_name':'last',
-            'password':'password'
-        }
-        return super().setUp()
-
-
-class SignupAPIViewTest(BaseTest):
-
-   def test_can_view_page_correctly(self):
-       response=self.client.get(self.signup_url)
-       self.assertEqual(response.status_code,200)
-
-   def test_can_register_user(self):
-        response=self.client.post(self.signup_url,self.user,format='text')
-        self.assertEqual(response.status_code,302)
-   
-   def test_cant_register_user_with_unmatching_passwords(self):
-        response=self.client.post(self.signup_url,self.user_unmatching_password,format='text')
-        self.assertEqual(response.status_code,400)
-
-   def test_cant_register_user_with_invalid_email(self):
-        response=self.client.post(self.signup_url,self.user_invalid_email,format='text')
-        self.assertEqual(response.status_code,400)
-
-   def test_cant_register_user_with_taken_email(self):
-        self.client.post(self.signup_url,self.user,format='text')
-        response=self.client.post(self.signup_url,self.user,format='text')
-        self.assertEqual(response.status_code,400)
+class SignupTestCase(APITestCase):
+    def test_signup(self):
+        data = {"email":"test@localhost.app","username":"testcase","first_name":"test","last_name":"case","password":"some_strong_psw"}
+        response = self.client.post("/auth/signup/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
