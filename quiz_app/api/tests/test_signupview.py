6c7119bcb071fc8d4b1c723fef95c26fb9fc49ff
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-08-05 07:38:27 +0300

    create a base test for signup view

--- quiz_app/api/tests/test_signupview.py
+++ quiz_app/api/tests/test_signupview.py
@@ -3,7 +3,45 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from authentication.utils import generate_token
 
-class RegisterTest(BaseTest):
+class BaseTest(TestCase):
+    def setUp(self):
+        self.signup_url=reverse('signup')
+        self.login_url=reverse('login')
+        self.user={
+            'email':'testemail@gmail.com',
+            'username':'username',
+            'password':'password',
+            'password2':'password',
+            'name':'fullname'
+        }
+        self.user_short_password={
+            'email':'testemail@gmail.com',
+            'username':'username',
+            'password':'tes',
+            'password2':'tes',
+            'name':'fullname'
+        }
+        self.user_unmatching_password={
+
+            'email':'testemail@gmail.com',
+            'username':'username',
+            'password':'teslatt',
+            'password2':'teslatto',
+            'name':'fullname'
+        }
+
+        self.user_invalid_email={
+            
+            'email':'test.com',
+            'username':'username',
+            'password':'teslatt',
+            'password2':'teslatto',
+            'name':'fullname'
+        }
+        return super().setUp()
+
+class SignupAPIViewTest(BaseTest):
+
    def test_can_view_page_correctly(self):
        response=self.client.get(self.register_url)
        self.assertEqual(response.status_code,200)
